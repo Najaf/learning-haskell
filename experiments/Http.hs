8e6e08df247a954a9d06b9fca45234fa49b1b9bd
Najaf Ali <ali.najaf@gmail.com>
2012-08-21 20:13:52 +0100

    added web server

--- experiments/Http.hs
+++
@@ -1,4 +0,0 @@
-import Network.HTTP
-
-main = do blah <- simpleHTTP (getRequest "http://api.twitter.com/1/users/show.json?screen_name=alinajaf") >>= fmap (take 1000) .  getResponseBody
-          putStrLn blah
